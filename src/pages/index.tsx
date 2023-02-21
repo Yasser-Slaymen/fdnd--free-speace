385ed9ff73be619ab5bb13ef6e4687268ec93faf
Yasser-Slaymen <yaserslyman0@gmail.com>
2023-02-21 14:43:19 +0100

    update section1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <HomeSecond />
         <HomeFirst />
+        <HomeSecond />
         <HomeThird />
       </main>
     </>
