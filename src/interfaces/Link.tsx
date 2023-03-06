23b02f78526e5b56278ec458e1cfe8be3c94d9d1
Yasser-Slaymen <yaserslyman0@gmail.com>
2023-03-06 21:49:16 +0100

    Update Link.tsx

--- src/interfaces/Link.tsx
+++ src/interfaces/Link.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 interface LinknavProps {
   link: string;
   content: string;
-  className:string;
+  className: string;
 }
 
-export default function Linknav({ link, content, className}: LinknavProps) {
+export default function Linknav({ link, content, className }: LinknavProps) {
   return (
     <>
-      <Link className={className} href={link} >{content}</Link>
+      <Link className={className} href={link}>
+        {content}
+      </Link>
     </>
   );
 }
